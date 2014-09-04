af69bfaf28d475fd490b8656c89b3ab54741d401
Benjamin Eidelman <beneidel@gmail.com>
2014-09-03 21:07:40 -0300

    bump from a fork

--- src/gulp/tasks/bump.js
+++ src/gulp/tasks/bump.js
@@ -3,16 +3,40 @@ exports.tags = [];
 exports.register = function(gulp, loader){
 
   var projectRoot = loader.projectRoot;
-  var path = require('path');
   require('shelljs/global');
   /*global config, exec, cd */
   config.fatal = true;
+  config.silent = true;
 
   var ghPages = require('../../gh-pages/publisher');
 
   gulp.task('bump', [], function(callback) {
     var packageInfo = loader.packageInfo();
     cd(projectRoot);
+    var remotes = exec('git remote').output.split('\n').filter(function(name){
+      return !!name;
+    });
+
+    var currentBranch = exec('git rev-parse --abbrev-ref HEAD').output.split('\n')[0];
+
+    var upstreamRemote = remotes.indexOf('upstream') >= 0 ? 'upstream' : 'origin';
+    var masterRemote = exec('git config branch.master.remote').output.split('\n')[0];
+    if (upstreamRemote !== masterRemote) {
+      // this is probably a fork, try to bump upstream
+      var upstreamMasterRemote = exec('git config branch.upstream-master.remote').output.split('\n')[0];
+      console.log('checking out ' + upstreamMasterRemote + ' master to bump package version');
+      if (upstreamMasterRemote) {
+        exec('git checkout upstream-master');
+      } else {
+        exec('git checkout -b upstream-master ' + upstreamRemote + '/master');
+      }
+    } else {
+      console.log('checking out master to bump package version');
+      exec('git checkout master');
+    }
+    console.log('fetching last version');
+    exec('git fetch ' + upstreamRemote);
+    exec('git rebase ' + upstreamRemote + '/master');
 
     // version bump
     exec('npm version patch');
@@ -38,6 +62,12 @@ exports.register = function(gulp, loader){
       callback();
     }
     exec('npm publish');
+
+    if (currentBranch !== 'master' && currentBranch !== 'HEAD') {
+      console.log('going back to branch ' + currentBranch);
+      exec('git checkout ' + currentBranch);
+    }
+
     callback();
   });
 
